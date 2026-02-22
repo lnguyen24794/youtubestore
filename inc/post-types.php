8ebbdcfab928688378fbcc077b8c0b72cac937d7
lam.nguyen <lnguyen24794@gmail.com>
2026-02-22 13:48:27 +0700

    Update post-types.php

--- inc/post-types.php
+++ inc/post-types.php
@@ -38,9 +38,40 @@ function youtubestore_register_cpt()
         'items_list_navigation' => __('Channels list navigation', 'youtubestore'),
         'filter_items_list' => __('Filter channels list', 'youtubestore'),
     );
+    $cpt_desc = __('YouTube Channels for sale', 'youtubestore');
+
+    $siteseo_options = get_option('siteseo_titles_option_name');
+    if (is_array($siteseo_options) && !empty($siteseo_options['titles_pt_archive_youtube_channel_desc'])) {
+        $cpt_desc = $siteseo_options['titles_pt_archive_youtube_channel_desc'];
+        if (function_exists('siteseo_parse_variables')) {
+            $cpt_desc = siteseo_parse_variables($cpt_desc);
+        } else {
+            $replacements = array(
+                '%%sitetitle%%' => get_bloginfo('name'),
+                '%%tagline%%' => get_bloginfo('description'),
+                '%%cpt_plural%%' => 'Danh sách Giới Thiệu Youtube',
+                '%%sep%%' => '-',
+            );
+            $cpt_desc = str_replace(array_keys($replacements), array_values($replacements), $cpt_desc);
+        }
+    } elseif (class_exists('WPSEO_Options')) {
+        $yoast_desc = WPSEO_Options::get('metadesc-ptarchive-youtube_channel', '');
+        if (!empty($yoast_desc)) {
+            $cpt_desc = function_exists('wpseo_replace_vars') ? wpseo_replace_vars($yoast_desc, null) : $yoast_desc;
+        }
+    } elseif (class_exists('RankMath')) {
+        $rm_options = get_option('rank-math-options-titles');
+        if (is_array($rm_options) && !empty($rm_options['pt_youtube_channel_archive_description'])) {
+            $cpt_desc = $rm_options['pt_youtube_channel_archive_description'];
+            if (function_exists('rank_math_replace_variables')) {
+                $cpt_desc = rank_math_replace_variables($cpt_desc);
+            }
+        }
+    }
+
     $args = array(
         'label' => __('YouTube Channel', 'youtubestore'),
-        'description' => __('YouTube Channels for sale', 'youtubestore'),
+        'description' => $cpt_desc,
         'labels' => $labels,
         'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
         'taxonomies' => array('category'),
