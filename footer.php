2b7befebd7916f8213bce396809c257346d3ac2f
lam.nguyen <lnguyen24794@gmail.com>
2026-02-23 08:52:51 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -68,9 +68,11 @@
                         <div class="fb-page" data-href="https://www.facebook.com/Youtubestore.vn" data-tabs="timeline"
                             data-width="" data-height="200" data-small-header="true" data-adapt-container-width="true"
                             data-hide-cover="true" data-show-facepile="true">
-                            <blockquote cite="https://www.facebook.com/Youtubestore.vn" class="fb-xfbml-parse-ignore"><a
-                                    href="https://www.facebook.com/Youtubestore.vn">Youtubestore.vn</a>
-                            </blockquote>
+                            <iframe
+                                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FYoutubestore.vn&tabs=&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                                width="340" height="130" style="border:none;overflow:hidden" scrolling="no"
+                                frameborder="0" allowfullscreen="true"
+                                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                         </div>
                     </div>
                 </div>
